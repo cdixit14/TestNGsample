9059282ac28fb4a882aa3537480a41cfce459036
chiragdixit74@gmail.com <emailid>
2024-04-25 07:34:27 +0000

    TestNGDAY1

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,30 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GooglePageTest {
+  @Test
+  public void JavaTutorialsearch() {
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://www.google.com/");
+	  WebElement srcBox = driver.findElement(By.className("gLFyf"));
+	  srcBox.sendKeys("Java Tutorial");
+	  srcBox.submit();
+	 
+  }
+  @Test
+  public void JavaTutorialsearch1() {
+  WebDriver driver = new ChromeDriver();
+  driver.manage().window().maximize();
+  driver.get("https://www.google.com/");
+  WebElement srcBox = driver.findElement(By.className("gLFyf"));
+  srcBox.sendKeys("W3school");
+  srcBox.submit();
+  }
+}
